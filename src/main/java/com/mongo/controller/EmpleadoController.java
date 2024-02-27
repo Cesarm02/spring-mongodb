8f49b2be663fffb8d5e6184c0d5b37b5d9739f8d
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2024-02-27 12:40:54 -0500

    Boostrap

--- src/main/java/com/mongo/controller/EmpleadoController.java
+++ src/main/java/com/mongo/controller/EmpleadoController.java
@@ -3,6 +3,8 @@ package com.mongo.controller;
 import com.mongo.service.IEmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,12 @@ public class EmpleadoController {
     @Autowired
     private IEmpleadoService iEmpleadoService;
 
+    @GetMapping
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("list", iEmpleadoService.buscar());
+        return "index";
+    }
 
 
 }
